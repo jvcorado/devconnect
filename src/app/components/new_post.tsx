0e1186028a2728c3037a54c3c5400079aea869a4
jvcorado <jhonisilva545@gmail.com>
2024-10-25 13:40:36 -0300

    Post

--- src/app/components/new_post.tsx
+++ src/app/components/new_post.tsx
@@ -6,6 +6,9 @@ import { UserButton, useUser } from "@clerk/clerk-react";
 import { useEffect, useRef } from "react";
 import { Textarea } from "@nextui-org/input";
 import create from "../api/create";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
 
 /* interface Posts {
   user_id: number;
@@ -15,6 +18,17 @@ import create from "../api/create";
   shares: number;
 }
  */
+
+const postsSchema = z.object({
+  user_id: z.number().int().nonnegative(),
+  content: z.string().min(1, "Content cannot be empty"),
+  image_url: z.string().url("Invalid URL format"),
+  likes: z.number().int().nonnegative(),
+  shares: z.number().int().nonnegative(),
+});
+
+export type PostsInputSchema = z.infer<typeof postsSchema>;
+
 export default function NewPost({
   newPost,
   setNewPost,
@@ -25,16 +39,28 @@ export default function NewPost({
   const { user, isSignedIn } = useUser();
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
-  const createPost = async () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+    watch,
+    setValue,
+  } = useForm<PostsInputSchema>({ resolver: zodResolver(postsSchema) });
+
+  const onSubmit = async () => {
+    const post = watch("content");
+
     let body = {};
     body = {
       user_id: 1,
-      content: "Compartilhando nosso primeiro post! na aplicação",
+      content: post,
       image_url:
         "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
       likes: 0,
       shares: 0,
     };
+
+    console.log(body);
 
     const response = await create({ path: "post/create", body });
 
@@ -51,24 +77,33 @@ export default function NewPost({
     }
   };
 
+  console.log(watch("content"));
+
   const handleKeyDown = (event: React.KeyboardEvent) => {
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault();
-      return createPost();
+      return onSubmit();
     }
   };
 
   const writePost = () => (
-    <Textarea
-      variant="bordered"
-      color="secondary"
-      ref={inputRef}
-      onKeyDown={handleKeyDown}
-      label=""
-      placeholder="Enter your post"
-      defaultValue={""}
-      className="w-[100%] mx-auto text-white"
-      autoFocus
+    <Controller
+      control={control}
+      name="content"
+      render={({ field }) => (
+        <Textarea
+          {...field}
+          variant="bordered"
+          color="secondary"
+          ref={inputRef} // Corrigido de 'ref' para 'inputRef'
+          onKeyDown={handleKeyDown}
+          label=""
+          placeholder="Enter your post"
+          defaultValue=""
+          className="w-[100%] mx-auto text-white"
+          autoFocus
+        />
+      )}
     />
   );
 
@@ -86,7 +121,13 @@ export default function NewPost({
             newPost ? "ps-5 " : "ps-5"
           }`}
         >
-          <div className="flex gap-5 items-center">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit((data) => onSubmit(data))();
+            }}
+            className="flex gap-5 items-center"
+          >
             <UserButton />
             <div className="flex flex-col gap-1 items-start justify-center">
               <h4 className="text-small font-semibold leading-none text-[#ffffff]">
@@ -96,7 +137,7 @@ export default function NewPost({
                 Write your post
               </h5>
             </div>
-          </div>
+          </form>
 
           <Button
             onPress={() => {
