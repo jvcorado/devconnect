9492a1866124b2af1702cfe70da6a591f38f3b16
jvcorado <jhonisilva545@gmail.com>
2024-11-10 12:29:14 -0300

    build

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,16 +2,15 @@
 
 import { useAuth } from "@/context/authContext";
 import React from "react";
-import ReactPullToRefresh from "react-pull-to-refresh";
+/* import ReactPullToRefresh from "react-pull-to-refresh"; */
 import { useMediaQuery } from "usehooks-ts";
 
 export default function Container({
   openModal,
-  handleRefresh,
   children,
 }: {
   openModal?: boolean;
-  handleRefresh: () => Promise<void>;
+  handleRefresh?: () => Promise<void>;
   children: React.ReactNode;
 }) {
   const isDesktop = useMediaQuery("(min-width: 768px)");
@@ -21,19 +20,19 @@ export default function Container({
 
   return (
     <div className="flex flex-col gap-2 md:gap-3 relative ">
-      <ReactPullToRefresh onRefresh={handleRefresh}>
-        <div
-          className={`${
-            isSignedIn
-              ? openModal && isDesktop
-                ? "md:max-h-[calc(100vh_-_320px)] md:min-h-[calc(100vh_-_320px)]  max-h-screen max-sm-pb-[250px] !overflow-y-auto"
-                : "md:max-h-[calc(100vh_-_180px)] md:min-h-[calc(100vh_-_180px)]  max-h-screen max-sm-pb-[250px] !overflow-y-auto"
+      {/*       <ReactPullToRefresh onRefresh={handleRefresh}> */}
+      <div
+        className={`${
+          isSignedIn
+            ? openModal && isDesktop
+              ? "md:max-h-[calc(100vh_-_320px)] md:min-h-[calc(100vh_-_320px)]  max-h-screen max-sm-pb-[250px] !overflow-y-auto"
               : "md:max-h-[calc(100vh_-_180px)] md:min-h-[calc(100vh_-_180px)]  max-h-screen max-sm-pb-[250px] !overflow-y-auto"
-          } flex flex-col gap-3 w-full text-white mx-auto  overflow-y-auto scroll-transparent  md:max-w-[60%] lg:max-w-[30%] h-full`}
-        >
-          {children}
-        </div>
-      </ReactPullToRefresh>
+            : "md:max-h-[calc(100vh_-_180px)] md:min-h-[calc(100vh_-_180px)]  max-h-screen max-sm-pb-[250px] !overflow-y-auto"
+        } flex flex-col gap-3 w-full text-white mx-auto  overflow-y-auto scroll-transparent  md:max-w-[60%] lg:max-w-[30%] h-full`}
+      >
+        {children}
+      </div>
+      {/*    </ReactPullToRefresh> */}
     </div>
   );
 }
